a40325f06e5343803c22e5e1b4c0b15b0a3c4680
shiv <shivshankarbagal1116@gmail.com>
2024-06-26 20:33:12 +0530

    CRUD operation

--- SpringBoot/spring-boot-jpa-example/src/main/java/com/javatpoint/SpringBootJpaExampleApplication.java
+++ SpringBoot/spring-boot-jpa-example/src/main/java/com/javatpoint/SpringBootJpaExampleApplication.java
@@ -1,5 +1,10 @@
 package com.javatpoint;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
 import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,16 +27,48 @@ public class SpringBootJpaExampleApplication {
 //User save = userRepository.save(user);
 //System.out.println(save);
 
-		User user1 = new User();
-		user1.setUsername("Akash");
-		user1.setCity("pune");
+//		User user1 = new User();
+//		user1.setUsername("Vaishnavi");
+//		user1.setCity("Nashik");
+//
+//		User user2 = new User();
+//		user2.setUsername("Rani");
+//		user2.setCity("Mumbai");
 
-		User user2 = new User();
-		user2.setUsername("Rani");
-		user2.setCity("Mumbai");
+//		User resultUser1 = userRepository.save(user1);
+//		System.out.println(resultUser1);
+
+//		//same multiple objects
+//		List<User> list = List.of(user1,user2);
+//		Iterable<User> finalResult = userRepository.saveAll(list);
+//		
+//		finalResult.forEach(a ->{
+//			System.out.println(a);
+//		});
+
+		// update
+
+//		Optional<User> optional = userRepository.findById(2);
+//		
+//		User user = optional.get();
+//		
+//		user.setCity("Nagpur");
+//		User save = userRepository.save(user);
+//		System.out.println(save);
+
+		// get the data
+//		Optional<User> optional = userRepository.findById(3);
+//		System.out.println(optional);
+
+		// get all data
+		Iterable<User> itr = userRepository.findAll();
+
+		itr.forEach(user -> {
+			System.out.println(user);
+		});
 		
-		User resultUser1 = userRepository.save(user1);
-		System.out.println(resultUser1);
+		//delete
+		userRepository.deleteById(4);
 
 	}
 }
